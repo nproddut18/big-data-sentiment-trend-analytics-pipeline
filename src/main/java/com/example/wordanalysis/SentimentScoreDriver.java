cd8c317a1cb88cdf0cf74c209468a71ea9bd0b6f
Naren Prodduturi <nproddut18@gmail.com>
2026-05-28 19:35:23 -0400

    Add src/main/java/com/example/wordanalysis/SentimentScoreDriver.java

---
+++ src/main/java/com/example/wordanalysis/SentimentScoreDriver.java
@@ -0,0 +1,33 @@
+package com.example.wordanalysis;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class SentimentScoreDriver {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: SentimentScoreDriver <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Sentiment Scoring");
+
+        job.setJarByClass(SentimentScoreDriver.class);
+        job.setMapperClass(SentimentScoreMapper.class);
+        job.setReducerClass(SentimentScoreReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
